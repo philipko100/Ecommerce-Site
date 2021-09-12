cd4111fca3ab7dfca8bb5a9c52a15ed57cef93f1
Yungu Philip Ko <philip.ko.100@gmail.com>
2021-09-12 01:40:12 -0700

    refactored code by creating a separate context processor file that makes categories available for all templates and added Product Manager in models to filter non active items

--- store/views.py
+++ store/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
-def categories(request):
-    return {
-        'categories' : Category.objects.all()
-    }
-
-def all_products(request):
-    products = Product.objects.all() # query
+def product_all(request):
+    products = Product.products.all() # query
     return render(request, 'store/home.html', {'products': products})
 
 def product_detail(request, slug):
