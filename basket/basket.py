0c470ace36414673161278a277a7b0416094ac71
Yungu Philip Ko <philip.ko.100@gmail.com>
2021-09-13 00:20:34 -0700

    created add to cart/basket functionality to purhcase items

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,5 @@
-
+from decimal import Decimal
+from store.models import Product
 
 class Basket():
 
@@ -6,6 +7,37 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number': 123123123}
+            basket = self.session['skey'] = {}
         self.basket = basket
+
+    # returns all items in basket
+    def __iter__(self):
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    # returns the quantity of items in baskeyt
+    def __len__(self):
+        return sum(item['qty'] for item in self.basket.values())
+
+    def save(self):
+        self.session.modified = True
+
+    # adds product to the basket
+    def add(self, product, qty):
+        product_id = str(product.id)
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] += qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
+        self.save()
+        
 
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .basket import Basket
+from store.models import Product
+from django.http import JsonResponse
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, qty=product_qty)
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
+        return response
