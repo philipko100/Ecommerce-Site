5e56dc161f4cf009ff4495f789bc2f9a6767d682
Yungu Philip Ko <philip.ko.100@gmail.com>
2021-09-14 03:24:15 -0700

    redirect to activation link after registration

--- account/views.py
+++ account/views.py
@@ -46,14 +46,18 @@ def account_register(request):
             user.save()
             current_site = get_current_site(request)
             subject = 'Activate your Account'
-            message = render_to_string('account/registration/account_activation_email.html', {
+            messageObject = {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': account_activation_token.make_token(user),
-            })
+            }
+            message = render_to_string('account/registration/account_activation_email.html', messageObject)
             user.email_user(subject=subject, message=message)
-            return HttpResponse('registered succesfully and activation sent')
+            return render(request, 'account/registration/account_activation_email.html', {'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user)})
     else:
         registerForm = RegistrationForm()
     return render(request, 'account/registration/register.html', {'form': registerForm})
