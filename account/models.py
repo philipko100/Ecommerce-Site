ec3bbbbd06e077f810b34563416e103b96b2725d
Yungu Philip Ko <philip.ko.100@gmail.com>
2021-09-14 02:42:02 -0700

    added user login, register, and log out functionality, with verification email functionality that is sent through the console

--- account/models.py
+++ account/models.py
@@ -53,6 +53,15 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = "Accounts"
         verbose_name_plural = "Accounts"
+    
+    def email_user(self, subject, message):
+        send_mail(
+            subject,
+            message,
+            'l@1.com',
+            [self.email],
+            fail_silently=False,
+        )
 
     def __str__(self):
         return self.user_name
