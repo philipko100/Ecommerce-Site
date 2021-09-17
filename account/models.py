6c98e51bdb553564d99448f097394fad42fcf9b0
Yungu Philip Ko <philip.ko.100@gmail.com>
2021-09-17 01:42:44 -0700

    create requirements.txt file and added documentation on what packages are required for this project

--- account/models.py
+++ account/models.py
@@ -1,8 +1,12 @@
-from django.db import models
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.core.mail import send_mail
+from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django_countries.fields import CountryField
+
 
 class CustomAccountManager(BaseUserManager):
 
@@ -34,7 +38,6 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=150, blank=True)
     about = models.TextField(_('about'), max_length=500, blank=True)
     # Delivery details
-    country = CountryField()
     phone_number = models.CharField(max_length=15, blank=True)
     postcode = models.CharField(max_length=12, blank=True)
     address_line_1 = models.CharField(max_length=150, blank=True)
